2f3b889cba1f2dbbcc66d10ba9266ec94c17daa0
huahua <47920763@qq.com>
2022-04-29 14:25:14 +0800

    init

---
+++ src/main/java/com/example/testgit/UserController.java
@@ -0,0 +1,12 @@
+package com.example.testgit;
+
+public class UserController {
+    private String getName(){return "xx";}
+    private Integer getAge() {
+        return 25;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("1");
+    }
+}
